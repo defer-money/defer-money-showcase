fd92ceee972981fdc61cb39de0d616081ea77099
Roshan Abhishek <roshan@eximpe.com>
2025-01-04 22:06:36 +0530

    Introduction Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,10 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors:{
-      'dark-blue': '#050020'
+    extend: {
+      colors:{
+        'dark-custom-blue': '#050020'
+      },
     },
     fontFamily: {
       sans: ['Inter', 'sans-serif'], // Use Inter as the default sans-serif font
