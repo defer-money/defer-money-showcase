a1dde086c304da810db3c8c50ead65d4aa43e1e1
Roshan Abhishek <roshan@eximpe.com>
2025-01-04 15:53:54 +0530

    Welcome Section

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,22 @@
 
 <template>
     <main class="flex flex-1 h-full">
-      <section id="welcome" class="flex-1 h-full">
-            <p class="font-bold text-[2rem] align-middle">defer.money</p>
+      <section id="welcome" class="flex flex-1 flex-col h-screen">
+          <div class="flex justify-between h-[7rem] mx-[2rem] items-center px-[1.5rem] py-[2rem]">
+            <p class="text-center font-bold text-[2rem]">defer.money</p>
+            <button class="bg-gradient-to-r from-[#763AF5] to-[#A604F2] rounded-full h-[4.5rem] w-[18rem] font-semibold text-[1.25rem]">Join the Waiting List</button>
+          </div>
+          <div class="flex flex-1 flex-col w-full h-full items-center justify-center gap-12">
+            <div class="gap-4 items-center justify-center flex flex-col">
+              <p class="font-normal text-[1.4rem] tracking-[0.25rem]">Welcome to new era</p>
+              <h1 class="font-extrabold text-[3.5rem]">Take Control of Your Payments</h1>
+              <div class="flex flex-col justify-center items-center">
+                <p class="font-normal text-[1.25rem] tracking-[0.08rem]">Defer your payments to the next billing cycle. </p>
+                <p class="font-bold text-[1.25rem] tracking-[0.08rem]">Save interest, avoid fees, and stay stress-free.</p>
+              </div>
+            </div>
+            <button class="bg-gradient-to-r from-[#763AF5] to-[#A604F2] rounded-full h-[4.5rem] w-[18rem] font-semibold text-[1.25rem]">Join the Waiting List</button>
+        </div>
       </section>
     </main>
     <footer class="text-center w-full">
