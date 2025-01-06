58a4cf31bfc87f74bb666178e195ecdd0c3de0df
Roshan Abhishek <roshan@eximpe.com>
2025-01-06 06:16:44 +0530

    Early Access Form

---
+++ src/components/EarlyAccessForm.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <!-- Form -->
+    <section id="early-access-form" class="flex flex-col gap-12 md:mx-12 md:mt-24 md:mb-12">
+        <div class="flex flex-col items-center">
+          <p class="font-extrabold text-[2rem] md:text-[4rem] text-white tracking-tight">Get Earl<span class="bg-gradient-to-r from-[#FFFFFF] from-0% via-[#763AF5] via-48% to-[#A604F2] to-99% bg-clip-text text-transparent">y Access</span></p>
+          <p class="font-medium text-base md:text-2xl text-center text-white text-opacity-70 tracking-tight">Join the waiting list to experience payment freedom with Defer.Money</p>
+        </div>
+        <div class="flex w-full bg-white bg-opacity-5 md:p-6 gap-6">
+          <div class="flex flex-col w-full p-6 gap-10 justify-center">
+            <div>
+              <p class="text-white text-[1.90rem] font-semibold tracking-tight">Fill out the form below</p>
+              <p class="text-gray-400 text-lg font-normal tracking-tight">We will get back to you once we are live!</p>
+            </div>
+            <div class="flex flex-col gap-6">
+              <div class="flex gap-4 w-full">
+                <input placeholder="First Name" class="bg-white bg-opacity-5 p-3 w-full rounded-lg text-white text-opacity-60" />
+                <input placeholder="Last Name" class="bg-white bg-opacity-5 p-3 w-full rounded-lg text-white text-opacity-60" />
+              </div>
+              <input placeholder="PhoneNumber" class="bg-white bg-opacity-5 p-3 w-full rounded-lg text-white text-opacity-60" />
+              <input placeholder="Email Address" class="bg-white bg-opacity-5 p-3 w-full rounded-lg text-white text-opacity-60" />
+              <button class="bg-gradient-to-r from-[#763AF5] to-[#A604F2] rounded-full h-[4.5rem] w-full font-semibold text-[1.25rem]">Submit</button>
+            </div>
+          </div>
+          <img src="../assets/early-access.png" alt="Background" class="hidden lg:block md:max-h-[35rem] md:w-full object-fill" />
+        </div>
+    </section>
+</template>
+
+<style scoped>
+</style>
