18d4762d686453233952488b5ae3f7ae2cf2cfba
Roshan Abhishek <roshan@eximpe.com>
2025-01-06 06:16:04 +0530

    Add Key Feature Section

---
+++ src/components/DeferKeyFeatures.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <!-- Why defer -->
+    <section id="how-defer-works" class="flex flex-col w-fit md:mx-12 my-12 md:my-24 gap-12 self-center">
+    <p class="font-extrabold text-[2rem] md:text-[2.7rem] text-white text-center">Why Defer.Money?</p>
+    <div class="w-full flex md:flex-row flex-col justify-between items-center gap-12">
+        <!-- Feature 1 -->
+        <div class="flex flex-col items-center text-center md:items-start md:text-start w-full min-h-[20rem] gap-9 p-9 bg-white bg-opacity-5">
+          <div class="w-[5.5rem] h-[5.5rem] m-0 p-0">
+              <img src="../assets/save-fee.png" alt="Background" class="w-full h-full object-cover" />
+          </div>
+          <div>
+              <p class="font-semibold text-[1.5rem] md:text-[2rem] text-white tracking-normal">Save on Fees</p>
+              <p class="font-normal text-[1rem] md:text-[1.25rem] text-gray-400 tracking-[-0.06rem]">Avoid high interest rates with a simple, low fee.</p>
+          </div>
+        </div>
+        <!-- Feature 2 -->
+        <div class="flex flex-col items-center text-center md:items-start md:text-start w-full min-h-[20rem] gap-9 p-9 bg-white bg-opacity-5">
+          <div class="w-[5.5rem] h-[5.5rem] m-0 p-0">
+              <img src="../assets/control.png" alt="Background" class="w-full h-full object-cover" />
+          </div>
+          <div>
+              <p class="font-semibold text-[1.5rem] md:text-[2rem] text-white tracking-normal">Stay in Control</p>
+              <p class="font-normal text-[1rem] md:text-[1.25rem] text-gray-400 tracking-[-0.06rem]">Make purchases without payment pressure.</p>
+          </div>
+        </div>
+        <!-- Feature 3 -->
+        <div class="flex flex-col items-center text-center md:items-start md:text-start w-full min-h-[20rem] gap-9 p-9 bg-white bg-opacity-5">
+          <div class="w-[5.5rem] h-[5.5rem] m-0 p-0">
+              <img src="../assets/financial-flexibility.png" alt="Background" class="w-full h-full object-cover" />
+          </div>
+          <div>
+              <p class="font-semibold text-[1.5rem] md:text-[2rem] text-white tracking-normal">Financial Flexibility</p>
+              <p class="font-normal text-[1rem] md:text-[1.25rem] text-gray-400 tracking-[-0.06rem]">Defer payments your way, stress-free.</p>
+          </div>
+        </div>
+    </div>
+    </section>
+</template>
+
+<style scoped>
+</style>
